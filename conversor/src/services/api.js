0a23282735dbcaf84cd8fde9f06f8e64faa60a86
jonatasimoes <janatadev.estudar@gmail.com>
2026-02-14 20:29:49 -0300

    Projetos do Curso

--- conversor/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// https://economia.awesomeapi.com.br/json/all
-
-// > Rota para buscar BTC > BRL : all/BTC-BRL
-
-export const api = axios.create({
-  baseURL: "https://economia.awesomeapi.com.br/json/"
-})
